fbcca7c9b0dcf09930a407736801fe84d03885c4
AttiaBut <attianoor543@gmail.com>
2023-10-22 14:39:36 +0600

    burger website

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: '#010a5e',
+        primaryColorLight: '010d78',
+        secondaryColorLight : '#FFCC00',
+        paragraphicColor :'#c0c0c0',
+        whiteColor : '#fff',
+        blackColor : '#000',
+        redColor : '#cc3433',
+        darkColor : '#000',
+        darkColorLight : '#171717',
+      }
+    },
+  },
+  plugins: [],
+}
+
